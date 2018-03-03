6bf41f0531b766a78eb0aecebdaaba2bdc0946c0
mohsen <alimohammadi340@gmail.com>
2018-03-02 19:56:19 -0500

    Config file changed

--- src/main/java/com/hoom/hoomcode/config/Config.java
+++ src/main/java/com/hoom/hoomcode/config/Config.java
@@ -2,6 +2,6 @@ package com.hoom.hoomcode.config;
 
 public class Config {
 
-    public static final String REDIS_HOST = System.getenv("REDIS_HOST") + ":6379";
+    public static final String REDIS_HOST = System.getenv("REDIS_HOST");
 
 }
